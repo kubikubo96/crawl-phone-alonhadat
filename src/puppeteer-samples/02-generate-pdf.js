5376afac21237c58e1bd6793a4ea81646777077e
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-09-28 22:43:24 +0700

    update crawl

--- src/puppeteer-samples/02-generate-pdf.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://github.com/');
-  await page.pdf({ path: 'github.pdf', format: 'A4' });
-  browser.close();
-})();
-
