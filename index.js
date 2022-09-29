f48911144399728c7e9b3ec49dae36936f5294f6
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-09-29 21:15:07 +0700

    "update crawl"

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import "dotenv/config";
     const page = await browser.newPage();
 
     //let ID = 792430;
-    let ID = 792558;
+    let ID = 792573;
     while (1) {
         try {
             const urlCrawl = 'https://alonhadat.com.vn/nha-moi-gioi/079-' + ID + '.html';
